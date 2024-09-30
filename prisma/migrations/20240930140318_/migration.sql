45a963a1844d2d2d8fde2ad2e12885534f79db3a
wasuken <wevorence@gmail.com>
2024-10-01 08:57:45 +0900

    追加、かつmysqlへ移行

---
+++ prisma/migrations/20240930140318_/migration.sql
@@ -0,0 +1,14 @@
+-- AlterTable
+ALTER TABLE `Post` ADD COLUMN `categoryId` INTEGER NULL;
+
+-- CreateTable
+CREATE TABLE `Category` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `Category`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
